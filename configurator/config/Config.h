2f95017dbfafcdcec3991733f9d2926ba1de4c55
ArturSalamatin <43352868+ArturSalamatin@users.noreply.github.com>
2022-05-02 14:32:22 +0300

    refactor: change codestyle

--- configurator/config/Config.h
+++ configurator/config/Config.h
@@ -3,9 +3,12 @@
 
 #include <filesystem>
 namespace SFPConfig {
+    /// 
     struct FileReplacmentConfig {
-        std::filesystem::path OnCreatorPath;
-        std::string RelativeUserPath;
+        // get a file from here
+        std::filesystem::path OnCreatorPath; // Creator creates a patch and describes where files are at Creator side
+        // put the file at this address
+        std::string RelativeUserPath; // where to store the file on unpack
     };
 }
 
